e895f5b318eb3c42b8eea387cd5b42009387e6e4
dcost <dcost@DC-SPECTRE>
2018-01-29 12:55:51 +0000

    Added foreach, imagebrush, refresh

--- ClassContainers/MainPage.xaml.cs
+++ ClassContainers/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
@@ -53,8 +54,8 @@ namespace ClassContainers
                     myEl = new Ellipse();
                     myEl.Name = "el_" + iRow + "_" + iCol;
                     myEl.Fill = new SolidColorBrush(Colors.RosyBrown);
-                    myEl.Height = 75;
-                    myEl.Width = 75;
+                    myEl.Height = 50;
+                    myEl.Width = 50;
                     myEl.SetValue(Grid.RowProperty, iRow);
                     myEl.SetValue(Grid.ColumnProperty, iCol);
                     // add event handler
@@ -68,10 +69,46 @@ namespace ClassContainers
         private void MyEl_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Ellipse curr = (Ellipse)sender;
-            curr.Fill = new SolidColorBrush(Colors.Red);
+            //'curr.Fill = new SolidColorBrush(Colors.Red);
+            ImageBrush brush = new ImageBrush();
+            // imagebrush is an object
+            // source is a bitmap image
+            // bitmap image uses a URI as a path
+            // can't find the file.
+            // need another way - exists in the appx
+            Uri uri = new Uri("ms-appx:///Images/Oogway.jpg", UriKind.RelativeOrAbsolute);
+            BitmapImage bitmap = new BitmapImage(uri);
+
+            brush.ImageSource = bitmap;
+            curr.Fill = brush;
+
+            // add an image here
+            // fill - object
+            // solid colour - solidcolorbrush
+            // image - imagebrush
             _iCount++;
             tblCount.Text = _iCount.ToString();
             curr.Tapped -= MyEl_Tapped;
+        }
+
+        private void elReset_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            // change all elipses back to rosy brown
+            // need some way of looping throug all ellipses
+            // on the grdBoard
+
+            foreach (var item in grdBoard.Children )
+            {
+                // only ellipses in this
+                if (item.GetType() == typeof(Ellipse))
+                {
+                    //((Ellipse)item).Fill = 
+                    //    new SolidColorBrush(Colors.RosyBrown);
+                    grdBoard.Children.Remove(item);
+                }
+            }
+
+
         }
     }
 }
